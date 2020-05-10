30111c7c62127596e481074c070dcde7a21ffe38
Eugene Leong <honghao@hotmail.sg>
2020-05-10 10:23:10 +1000

    Updates

---
+++ precache-manifest.57b4d3364a798066e8097093cf728fa3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea772b0c7b43266efa72397eafcdd16c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "7eeee8fbf9eaa99c94bd",
+    "url": "/React-Portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "478e47ae3e69c136ed8a",
+    "url": "/React-Portfolio/static/css/main.a99dcd75.chunk.css"
+  },
+  {
+    "revision": "7eeee8fbf9eaa99c94bd",
+    "url": "/React-Portfolio/static/js/2.d01d5e34.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.d01d5e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478e47ae3e69c136ed8a",
+    "url": "/React-Portfolio/static/js/main.583319bd.chunk.js"
+  },
+  {
+    "revision": "1bdac82cd996ae1ecb8f",
+    "url": "/React-Portfolio/static/js/runtime-main.e19ca9a5.js"
+  },
+  {
+    "revision": "e10bb3d4d327f5724f74d28d0f8678f4",
+    "url": "/React-Portfolio/static/media/NoteTaker.e10bb3d4.PNG"
+  },
+  {
+    "revision": "4d79fd10e7b85f497d3b5f8ad4af6ecc",
+    "url": "/React-Portfolio/static/media/PasswordGenerator.4d79fd10.PNG"
+  },
+  {
+    "revision": "453defe7d35b323532da7c10e3071891",
+    "url": "/React-Portfolio/static/media/SantasList.453defe7.PNG"
+  },
+  {
+    "revision": "e3f57282fb5709495a8a0c7334aa1151",
+    "url": "/React-Portfolio/static/media/TrafficApp.e3f57282.PNG"
+  },
+  {
+    "revision": "95fc30ed350943c65faa3e56a7d55f1c",
+    "url": "/React-Portfolio/static/media/WeatherDashboard.95fc30ed.PNG"
+  },
+  {
+    "revision": "f925b4c9ceebf6cedb95f6827c0c2794",
+    "url": "/React-Portfolio/static/media/WorkDaySchedular.f925b4c9.PNG"
+  },
+  {
+    "revision": "4b000138bc9737a255f2086ffb773443",
+    "url": "/React-Portfolio/static/media/profilepic.4b000138.jpg"
+  }
+]);
